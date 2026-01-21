a9dd4e5b41f2a515adec768db72b0f6bb6b39856
santhanaraja <santhanaraja2018@gmail.com>
2026-01-21 23:38:29 +0530

    Pyspark

---
+++ Rdd_Transformations.py
@@ -0,0 +1,44 @@
+from pyspark.sql import SparkSession
+spark = (SparkSession.builder.appName("MyApplication")
+                                     .config("spark.hadoop.io.native.lib.available", "false")
+                                     .config("spark.hadoop.native.lib", "false")
+                                     .config("spark.hadoop.fs.file.impl.disable.cache", "true").getOrCreate())
+
+
+#creating an RDD from a list of tuples
+data = [("Alice",25),("Bob",50),("Charlie",35),("Alice",45)]
+rdd = spark.sparkContext.parallelize(data)
+
+#Map transformation : convert name to upper
+mapped_rdd = rdd.map(lambda x : (x[0].upper(),x[1]))
+result = mapped_rdd.collect()
+print("\nRdd elements after capitilizing names \n",result)
+
+#filter transformation : filter records with where condition
+filtered_rdd_age = rdd.filter(lambda x : x[1]>30)
+result = filtered_rdd_age.collect()
+print("\nRdd elments with age > 30 : ",result)
+
+filtered_rdd_name= rdd.filter(lambda x : "bob" in x[0].lower())   #%like% in sql
+result = filtered_rdd_name.collect()
+print("\nRdd elments with name like bob : ",result)
+
+#Reduce key transformation : calculate the total age for each name
+reduced_rdd = rdd.reduceByKey(lambda x, y : x+y)
+result = reduced_rdd.collect()
+print("\nRdd elments with age added for each name : ",result)
+
+#sort by age in descending order
+sorted_rdd = rdd.sortBy(lambda x : x[1], ascending = False)
+result = sorted_rdd.collect()
+print("\nRdd elments sort by age in descending order : ",result)
+
+# to save rdds as text file
+rdd.saveAsTextFile("file:///C:/Users/SANTHANA RAJA S/Desktop/Git/PySpark/output.txt")
+
+#create or read rdd from text file, read is not working in our lap
+# rdd_text = spark.sparkContext.textFile("file:///C:/Users/SANTHANA RAJA S/Desktop/Git/PySpark/output.txt")
+# result = rdd_text.collect()
+# print("\nRdd elments from text file : ",result)
+
+spark.stop()
--- Rdds_Actions.py
+++ Rdds_Actions.py
@@ -3,20 +3,21 @@ spark = SparkSession.builder.appName("MyApplication").getOrCreate()
 
 
 #creating an RDD from a list of tuples
-data = [("Alice",25),("bob",50),("Charlie",35),("Alice",45)]
+data = [("Alice",25),("Bob",50),("Charlie",35),("Alice",45)]
 rdd = spark.sparkContext.parallelize(data)
+print("\n Rdd elements : \n",rdd)
 
 Count = rdd.count()
-print("Total number of elments in RDD : ",Count)
+print("\nTotal number of elments in RDD : ",Count)
 
 first = rdd.first()
-print("First Element : ",first)
+print("\nFirst Element : ",first)
 
 take_elements = rdd.take(2)
-print ("The elements taken : ",take_elements)
+print ("\nThe elements taken : ",take_elements)
 
 
-#for each action
+#for each action to print elements of rdd
 rdd.foreach(lambda x: print(x))
 
 
